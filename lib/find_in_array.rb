7dfa85e87048def056dec3571160df011144e5d4
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-02 21:08:36 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,3 @@
 def find_element_index(array, value_to_find)
-  counter = 0 
-  index_value = 0
-  while counter < array.length do
-    if array[counter] == value_to_find
-      index_value = counter
-    end 
-    counter += 1 
-  end
-  index_value
+  array.index(value_to_find)
 end
